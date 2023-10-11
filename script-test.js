53d1ed45977441b8192b615133bf6ab078c858f1
fpS-AUDIO <saudio.ru@gmail.com>
2023-10-11 17:38:12 +0200

    updating JS notes

--- script-test.js
+++ script-test.js
@@ -1,35 +1,16 @@
 "use strict";
 
-////////////////////////////////////////
-//// The Rest Pattern in JavaScript ////
-////////////////////////////////////////
-// Coding Note #20
-
-// You can use the rest pattern to collect all (or all remaining) elements into an array.
-// This is why it's called the "Rest Pattern" because it takes the rest of the elements
-// in the destructuring assignment and places them into a new array.
-
-// The rest pattern has the same syntax as the spread operator (...)
-// but is typically positioned on the left side of the "=" sign,
-// unlike the spread operator, which is usually positioned on the right side.
-
-// Basic syntax
-const [numberOne, numberTwo, ...allOtherNumbers] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(numberOne, numberTwo); // 1 2
-console.log(allOtherNumbers); // [3, 4, 5, 6, 7, 8, 9]
-
-// Example using a function
-// This function expects individual numbers as arguments
-const addAllNumbers = function (...allNumbers) {
-  let totalSum = 0;
-  for (const num of allNumbers) totalSum += num;
-  return totalSum;
+const flight = `MLN123`;
+const Alex = {
+  name: `Alex Smitt`,
+  passport: 123456789,
 };
 
-const arrayNumbers = [1, 2, 3, 4, 5];
-const result1 = addAllNumbers(...arrayNumbers);
-const result2 = addAllNumbers(2, 3, 4, 5, 6);
-console.log(result1); // 15
-console.log(result2); // 20
+const checkIn = function (fligntNumber, passenger) {
+  fligntNumber = `MLN999`;
+  passenger.name = `Mr.` + passenger.name;
+};
 
-// Follow @coding_feature on Instagram 👍
+checkIn(flight, Alex);
+console.log(flight); // didn't changed
+console.log(Alex); // name is changed:  name: 'Mr.Alex Smitt'
