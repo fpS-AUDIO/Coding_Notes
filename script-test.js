b4722ff4e3d58d841f62bde0d52049f4693de453
fpS-AUDIO <saudio.ru@gmail.com>
2023-10-14 17:31:53 +0200

    updating JS notes

--- script-test.js
+++ script-test.js
@@ -21,3 +21,45 @@ greet(`Goodmoring`)(`everybody`);
 const greetArrow = (greetStr) => (name) => console.log(`${greetStr} ${name}`);
 greetArrow(`Hi`)(`People`);
 // output:  Hi People
+
+///////////////////////////////////////////////////////////////
+console.log(`------------- For @coding_feature -------------`);
+///////////////////////////////////////////////////////////////
+
+////////////////////////////
+//// Sets in JavaScript ////
+////////////////////////////
+// Coding Note #22
+
+// Sets are collections of any data types but only unique values.
+// Sets are also iterables but they are unordered (there are no indexes).
+// Sets can be empty and they were introduced in ES6.
+// Sets can help you, for example, remove duplicates from an array
+
+// Creating a set:
+const setName1 = new Set([6, `pizza`, 3, `pizza`, 6, 5, `pasta`]);
+
+//  Creating an empty set using constructor
+const setName2 = new Set();
+console.log(setName2); // OUTPUT:  Set(0) {size: 0}
+
+console.log(setName1); // OUTPUT:  {6, 'pizza', 3, 5, 'pasta'}
+
+// Size property of a set:
+console.log(setName1.size); // OUTPUT:  5
+
+// Create a new array from a set using the spread operator
+const arrayFromSet = [...setName1];
+console.log(arrayFromSet); // OUTPUT: [6, 'pizza', 3, 5, 'pasta']
+
+//    Other set methods:
+setName1.add(8); // Adds the value 8 to the set
+const hasPizza = setName1.has("pizza"); // Returns true if 'pizza' is in the set
+setName1.delete(3); // Removes the value 3 from the set
+setName1.forEach((value) => console.log(value)); // Invokes a callback for each element
+const valuesIterator = setName1.values(); // Returns an iterator with all the values
+const keysIterator = setName1.keys(); //  Returns an iterator with all keys of the set
+const entriesIterator = setName1.entries(); // Returns an iterator with [value, value] pairs
+setName1.clear(); //  Removes all elements from a Set
+
+// Follow @coding_feature on Instagram 👍
