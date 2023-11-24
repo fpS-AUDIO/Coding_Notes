f990520724d9288963b61ac2cde7ace6787a45c6
fpS-AUDIO <saudio.ru@gmail.com>
2023-11-24 21:46:58 +0100

    updating JS notes

--- script-test.js
+++ script-test.js
@@ -4,31 +4,43 @@
 console.log(`---------------For @coding_feature---------------`);
 /////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////
-//// Using the array.findIndex() method in JavaScript ////
-//////////////////////////////////////////////////////////
-// Coding Note #42
+//////////////////////////////////////////////////////////////
+//// array.some() and array.every() methods in JavaScript ////
+//////////////////////////////////////////////////////////////
+// Coding Note #43
 
-// SYNTAX:  array.findIndex(callback, thisValue)
-//          array.findIndex(callback(element, index, array), thisValue)
+// SYNTAX:  array.some(callbackFn, thisValue)
+//          array.some(function (element, index, array), thisValue)
+//          array.every(function (element, index, array), thisValue)
 
-// The array.findIndex() method requires a callback function that should return a boolean.
-// If the returned boolean is true, the findIndex() method returns the index of that element.
-// The callback function can optionally accept three parameters: the current element, its index, and the entire array.
-// Note: If no values satisfy the testing function, the method returns -1.
-// Additionally, the second argument of findIndex() allows you to specify the value of the "this" keyword during execution.
+// The array.some() method tests if AT LEAST ONE element of an array passes the given test.
+// It requires a callback function as the first argument.
+// The callback function can accept 3 optional parameters: the current element, its index, and the entire array.
+// An evaluation follows the 'return' keyword of the callback function, returning a boolean.
+// The method returns 'true' if at least one element of the array passes the test in the callback function.
+// Additionally, you can provide the value of the 'this' keyword by passing it as the second argument of the some() method.
+// Importantly, the original array remains unaltered.
 
-const arrayNumbers = [20, 48, 3, -21, 52, -98, 2, -5];
+const arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
-// Searching for the index of the 1st negative number in the given array
-const resultIndex1 = arrayNumbers.findIndex(function (num) {
-  return num < 0; // Output: 3
+// Example:
+// Check if the array contains at least one even number
+const hasEven1 = arrayNumbers.some(function (num) {
+  return num % 2 === 0;
 });
-console.log(resultIndex1);
+console.log(hasEven1); // true
 
-// Same example but using the arrow function syntax
-const resultIndex2 = arrayNumbers.findIndex((num) => num < 0);
-console.log(resultIndex2); // Output: 3
+// Same example but using arrow function syntax
+const hasEven2 = arrayNumbers.some((num) => num % 2 === 0);
+console.log(hasEven2); // true
+
+// ------------------------------------------------------- //
+
+// The array.every() method is exactly the same as some() but it returns true only if ALL elements pass the condition.
+
+// Check if the array contains only even numbers
+const onlyEven = arrayNumbers.every((num) => num % 2 === 0);
+console.log(onlyEven); // false
 
 // Follow @coding_feature on Instagram for more content! 👍
 
