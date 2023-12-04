5b9f82672d048b835ae408896eeb24cb9043a0db
fpS-AUDIO <saudio.ru@gmail.com>
2023-12-04 21:05:33 +0100

    updating JS notes

--- script-test.js
+++ script-test.js
@@ -4,35 +4,26 @@
 console.log(`---------------For @coding_feature---------------`);
 /////////////////////////////////////////////////////////////////
 
-///////////////////////////////////////////////////////////////////////
-//// array.indexOf() and array.lastIndexOf() methods in JavaScript ////
-///////////////////////////////////////////////////////////////////////
-// Coding Note #52
+////////////////////////////////////////////
+//// Array.from() Method in JavaScript ////
+////////////////////////////////////////////
+// Coding Note #53
 
-// SYNTAX:        array.indexOf(searchElement, fromIndex).
-// The indexOf() method returns the first index of the `searchElement` passed as an argument.
-// It uses strict equality (===) when comparing elements.
-// Optionally, you can specify the start index (inclusive) for searching as the second argument.
-// If the element is not present, it returns -1.
+// SYNTAX: Array.from(arrayLike)
+// SYNTAX: Array.from(arrayLike, mapFunction(element, index, array), thisArg)
 
-const arrayLetters = [`a`, `b`, `c`, `a`, `b`, `c`];
+// The Array.from() method is used to create an array from an iterable or array-like object.
+// It is particularly useful, for example, when applying array methods to a NodeList obtained from querySelectAll().
+// Optionally, a mapping function (similar to the map() function for arrays) can be passed as the second argument.
+// Additionally, a value for the "this" keyword can be provided as the third parameter (default is undefined).
+// Note: This method is called on the Array object (not instances).
 
-// Example 1: Finding the first index of 'b'
-const firstIndex = arrayLetters.indexOf(`b`);
-console.log(firstIndex); // Output: 1
+// Example 1
+const arrayFromStr = Array.from(`coding`);
+console.log(arrayFromStr); // ['c', 'o', 'd', 'i', 'n', 'g']
 
-// Example 2: Finding the index of 'b' starting from index 2
-const secondIndex = arrayLetters.indexOf(`b`, 2);
-console.log(secondIndex); // Output: 4
-
-// ------------------------------------------------------- //
-
-// SYNTAX:        array.lastIndexOf(searchElement, fromIndex)
-// The lastIndexOf() method is similar to indexOf() but returns the last index of the element.
-// It searches the array backward, optionally starting from the specified `fromIndex`.
-
-// Example 3: Finding the last index of 'a'
-const lastIndex = arrayLetters.lastIndexOf(`a`);
-console.log(lastIndex); // Output: 3
+// Example 2: Creating an array of squared numbers
+const arrayFromObj = Array.from({ length: 5 }, (_, i) => i * i);
+console.log(arrayFromObj); // [0, 1, 4, 9, 16]
 
 // Follow @coding_feature on Instagram for more content! 👍
