f9ac3a67dec62956afb2a2e33e30953db61142a2
fpS-AUDIO <saudio.ru@gmail.com>
2024-01-28 22:53:26 +0100

    updating frontend notes and examples

---
+++ Frontend Examples and Components/example modal window using dialog/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const btnOpenModal = document.querySelectorAll(".open-modal-window");
+const btnCloseModal = document.querySelectorAll(".close-modal-window");
+const modalWindows = document.querySelectorAll(".modal-window");
+
+// btnOpenModal.addEventListener(`click`, () => {
+//   modalWindow.showModal();
+// });
+
+btnOpenModal.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    modalWindows[index].showModal();
+  });
+});
+
+btnCloseModal.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    modalWindows[index].close();
+  });
+});
+
+// to close on click
+const modal01 = document.getElementById(`dialog1`);
+
+modal01.addEventListener(`click`, (event) => {
+  const dialogDimensions = modal01.getBoundingClientRect();
+  if (
+    event.clientX < dialogDimensions.left ||
+    event.clientX > dialogDimensions.right ||
+    event.clientY < dialogDimensions.left ||
+    event.clientY > dialogDimensions.right
+  ) {
+    modal01.close();
+  }
+});
