17fe3caffe5d40e9b257b6dfc85cfb5a771fcd6b
fpS-AUDIO <saudio.ru@gmail.com>
2024-02-20 21:44:52 +0100

    added JS notes about classes' setters, getters, static methods and Object.create()

--- script-test.js
+++ script-test.js
@@ -1,53 +1,41 @@
 "use strict";
 
-///////////////////
-// Classes in JS //
-///////////////////
+///////////////
+// Challenge //
+///////////////
 
-/* Theory:
- -  Classes in JS are not working like traditional classes in other programming languages
- -  They are just syntactic sugar and they do the exact same thing as constructor function
-    with its prototypal inheritans but they're using a nicer and more modern syntax.
- -  Class are special type of functions so they can be expression and declaraytion
- -  Any type of class is not hoisted (even declaration)
- -  Classes are first-class citizes (you can pass classes like arguments and return them from functions)
- -  Body of class is always executed in strict mode
-*/
-
-// ---------------------------------------------------------------------------------------- //
-// --- syntax ---
-
-// class expression
-// const PersonClass = class {};
-
-// class declaration
-class PersonClass {
-  // `constructor` is the method of `class`
-  constructor(firstName, birthYear) {
-    // specify properties you wanna create in object
-    this.firstName = firstName;
-    this.birthYear = birthYear;
+class Car {
+  constructor(options) {
+    this.make = options.make;
+    this.speed = options.speed;
   }
 
-  // outside (below) the body of constructor() specify methods of .prototype
-  // note:  you don't need to use commas
-  calcAge() {
-    return 2024 - this.birthYear;
+  accelerate() {
+    this.speed += 10;
   }
 
-  hello() {
-    return `Hello ${this.firstName}`;
+  brake() {
+    this.speed -= 5;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(miH) {
+    this.speed = miH * 1.6;
   }
 }
 
-// when we create a new istance of class the constructor() is called automatically
-const alex = new PersonClass(`Alexander`, 1996);
-console.log(alex); // PersonClass {firstName: 'Alexander', birthYear: 1996}
-console.log(alex.calcAge()); // 28
-console.log(alex.hello()); // Hello Alexander
+const ford = new Car({
+  make: `Ford`,
+  speed: 120,
+});
 
-// proof that class acts like any function constructor
-console.log(alex.__proto__ === PersonClass.prototype); // true
+ford.accelerate();
+ford.brake();
+console.log(ford);
 
-// ---------------------------------------------------------------------------------------- //
-// --- setters and getters ---
+console.log(ford.speedUS);
+ford.speedUS = 80;
+console.log(ford);
